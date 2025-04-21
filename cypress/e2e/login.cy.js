f0448df7b0659744d5a9bcc7bff20c5453087496
unknown <dancardosogaldino@gmail.com>
2025-04-21 17:26:55 -0300

    melhorado o script add seletores

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,45 @@
 describe('Orange Hrm test', () => {
+
+  const  selectorList = {
+    userameField:"[name='username']",
+    passwordField:"[name='password']",
+    loginButton:"[type='submit']",
+    sectionTitleTopBar:".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert:".oxd-alert"
+
+  }
+
   it('login - sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    // Utilzando seletor como variavel, melhor forma de utilizar
+    cy.get(selectorList.userameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+  })
+
+  it('login - fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    // Seletores copiados diretamente do cypress
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+
+  it.skip('login - sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     // Seletor diferente afim de não deixar o script flake
     cy.get("[name='username']").type('Admin')
     cy.get("[name='password']").type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get("[type='submit']").click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
   })
-  it('login - fail', () => {
+  it.skip('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    // Seletores copiado diretamente do cypress
+    // Seletores copiados diretamente do cypress
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
     cy.get('.oxd-button').click()
